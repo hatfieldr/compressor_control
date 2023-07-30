4aa6800e36c3b8c29f143d98a77f1b95b0e13a0a
Nick Mortimer <nick.mortimer@csiro.au>
2023-07-31 05:48:22 +0800

    added Temp class

--- pressure.py
+++ pressure.py
@@ -1,35 +1,56 @@
 from ADCPi import ADCPi
+import adafruit_mcp9600
+import board
+import busio
+import numpy as np
 
 class Pressure:
     def __init__(self,channels,offsets,scales):
-        self.P[len(offsets)]
+        self.P = np.empty(1,len(offsets))
         self.adc = ADCPi(0x68, 0x69,12)
         self.offsets = offsets
         self.scales = scales
         self.channels = channels
-    def read_presures(self):
-        for index in range(0,self.channels):
-            self.P[index]=self.adc.read_voltage(self.channels[index])
+    def read(self):
+        for index,channel in enumerate(self.channels):
+            self.P[index]=self.adc.read_voltage(channel)
             self.P[index] = (self.P[index]-self.offsets[index])*self.scales[index]
-    def get_pressure_string(self,index):
+    def get_string(self,index):
         #if pressure < -5, sensor or loop is faulty
         if self.P[index] < -5:
             return "P Sensor FAULTY"
         else:
             return f"Pressure: {self.P[index]:.1f} Bar"
 
+class Temperature:
+    def __init__(self,addresses):
+        self.addresses = addresses
+        self.T = self.P = np.empty(1,len(addresses))
+        self.i2c = busio.I2C(board.SCL, board.SDA, frequency=100000)
+    def read(self):
+        for idx, address in enumerate(self.addresses):
+            self.T[idx] = adafruit_mcp9600.MCP9600(self.i2c,address=address).temperature
+    
+    def getstring(self,index):
+        return f'Temp: {self.T[index]} \u00b0C'
+
 
      
 if __name__ == '__main__':
     import time
     channels = range(1,5)
-    press = pressure(channels=channels,offsets=[0.481845,0.481845,0.481845,0.481845],
+    addresses=[0x66,0x65,0x64]
+    press = Pressure(channels=channels,offsets=[0.481845,0.481845,0.481845,0.481845],
                      scales=[25.941952,25.941952,25.941952,25.941952])
+    temp = Temperature(addresses=addresses)
     try:
         while True:
-            press.read_presures()
-            for channel in channels:
-                print(press.get_pressure_string(channel))  
+            press.read()
+            temp.read()
+            for idx,channel in enumerate(channels):
+                print(f'Pressure Channel: {channel} ={press.get_string(idx)}')  
+            for idx,address in enumerate(addresses):
+                print(f'Temp Address: {address} ={temp.get_string(idx)}')  
             time.sleep(1)      
     except KeyboardInterrupt:
         pass
